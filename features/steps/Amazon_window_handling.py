816a52c253d4a8f7e12477f1493aa1ceb41dd2d8
Yordanos Amare <yordiamare@yahoo.com>
2021-06-27 00:16:19 -0400

    Homework 6

---
+++ features/steps/Amazon_window_handling.py
@@ -0,0 +1,35 @@
+from selenium.webdriver.common.by import By
+from behave import given, then
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+
+
+@given('Open Amazon T&C page')
+def open_amazon_page(context):
+    context.driver.get('https://www.amazon.com/gp/help/customer/display.html/ref=ap_register_notification_condition_of_use?ie=UTF8&nodeId=508088')
+sleep(1)
+
+@when('Store original windows')
+def store_window(context):
+    context.original_window = context.driver.current_window_handle
+
+sleep(1)
+
+@when('Click on Amazon Privacy Notice link')
+def click_on_amazon_privacy_link(context):
+    context.driver.find_element(By.XPATH, "//a[@href='https://www.amazon.com/privacy']").click()
+
+sleep(1)
+
+@when('Switch to the newly opened window')
+def switch_to_the_new_window(context):
+    context.driver.wait.until(EC.new_window_is_opened)
+    new_window = context.driver.window_handles[1]
+    context.driver.switch_to_window(new_window)
+
+sleep(1)
+
+@then('Verify Amazon Privacy Notice page is opened')
+def verify_amazon_privacy_notice_page(context):
+    sleep(1)
+    context.driver.get('https://www.amazon.com/gp/help/customer/display.html?nodeId=GX7NJQ4ZB8MHFRNJ')
---
+++ features/tests/Amazon_window_handling.feature
@@ -0,0 +1,11 @@
+# Created by yordi at 6/26/2021
+Feature: Test for Amazon window handling
+
+  Scenario: User can open and close Amazon Privacy Notice
+    Given Open Amazon T&C page
+    When Store original windows
+    And Click on Amazon Privacy Notice link
+    And Switch to the newly opened window
+    Then Verify Amazon Privacy Notice page is opened
+
+
